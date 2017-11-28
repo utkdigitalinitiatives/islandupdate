546aed837514816f59e0369484c98e13350d1ff8
bridger <bdysonsmith@gmail.com>
2017-11-28 12:02:02 -0500

    address DIT-861: add SQL inserts for xml_form tables

---
+++ form_tables/xml_form_builder_form_associations.sql
@@ -0,0 +1,2 @@
+INSERT INTO `xml_form_builder_form_associations` (`id`, `content_model`, `form_name`, `dsid`, `title_field`, `transform`, `self_transform`, `template`) VALUES
+(1, 'islandora:sp_large_image_cmodel', 'Roth Form', 'MODS', 'a:2:{i:0;s:17:"titleInfosupplied";i:1;s:5:"title";}', 'mods_to_dc.xsl', 'roth_post_process.xsl', '');
